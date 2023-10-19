7ba87f77e8708aa01f2a21392b04884decc09e8b
zeeroiq <shrikantvishwakarma@outlook.com>
2023-10-20 01:31:30 +0530

    data_types - context object example. closes #7

--- HelloWorldFunction/src/main/java/com/zeero/DataTypesExample.java
+++ HelloWorldFunction/src/main/java/com/zeero/DataTypesExample.java
@@ -1,5 +1,7 @@
 package com.zeero;
 
+import com.amazonaws.services.lambda.runtime.Context;
+import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.zeero.pojo.Patient;
 import com.zeero.pojo.TestsData;
@@ -12,7 +14,13 @@ import java.util.stream.Collectors;
 
 import static java.lang.reflect.Modifier.TRANSIENT;
 
-public class ListDataType {
+public class DataTypesExample {
+
+    Gson gson = new GsonBuilder()
+            .excludeFieldsWithModifiers(TRANSIENT)
+            .excludeFieldsWithoutExposeAnnotation()
+            .setPrettyPrinting()
+            .create();
 
     public List<Integer> getSampleScore(List<String> names) {
         Map<String, Integer> studentScores = new HashMap<>();
@@ -46,12 +54,8 @@ public class ListDataType {
         student2scoreMap.put("DE", Arrays.asList(75, 80, 90));
 
 
-        String parsedName2ScoreMap = new GsonBuilder()
-                .excludeFieldsWithModifiers(TRANSIENT)
-                .excludeFieldsWithoutExposeAnnotation()
-                .setPrettyPrinting()
-                .create()
-                .toJson(student2scoreMap);
+
+         String parsedName2ScoreMap = gson.toJson(student2scoreMap);
         System.out.println("Student 2 Score map :\n" + parsedName2ScoreMap);
         return student2scoreMap;
     }
@@ -66,10 +70,25 @@ public class ListDataType {
         return testsData;
     }
 
-    public void testOutputStream(InputStream input, OutputStream output) throws IOException {
+    public void testOutputStream(InputStream input, OutputStream output, Context context) throws IOException {
         int data;
         while ((data = input.read()) != -1) {
             output.write(Character.toLowerCase(data));
         }
+        System.out.println("Remaining Time In Millis - " + context.getRemainingTimeInMillis());
+        System.out.println("Function Name - " + context.getFunctionName());
+        System.out.println("Function Version - " + context.getFunctionVersion());
+        System.out.println("Invoked Function ARN - " + context.getInvokedFunctionArn());
+        System.out.println("Memory Limit In MB - " + context.getMemoryLimitInMB());
+        System.out.println("AWS request ID - " + context.getAwsRequestId());
+        System.out.println("Log Group Name - " + context.getLogGroupName());
+        System.out.println("Log Stream Name - " + context.getLogStreamName());
+        System.out.println("Identity - " + context.getIdentity());
+        System.out.println("Client Context - " + context.getClientContext());
+        System.out.println("Logger - " + context.getLogger());
+        context.getLogger().log("Context Object info\n");
+        context.getLogger().log(gson.toJson(context));
+        System.out.println("\n");
+
     }
 }
